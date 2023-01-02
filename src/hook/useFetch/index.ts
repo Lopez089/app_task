d381e11f2ac96556d68522f1f024a8f2ead0a392
LopezDev <aragonlopezjuan87@gmail.com>
2023-01-02 22:43:06 +0100

    feat(server): :sparkles: crear hook useFetch
    crear un fetch donde retorna el stado de una peticion api
    
    #5

---
+++ src/hook/useFetch/index.ts
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import { task } from '../../interface'
+
+interface IStateFetch<T> {
+    state: 'idle' | 'loading' | 'error' | 'success',
+    data: null | T[]
+    error: null | Error
+}
+
+export const useFetch = (url: string) => {
+    const [stateFetch, setStateFetch] = useState<IStateFetch<task[]>>({
+        state: 'idle',
+        data: null,
+        error: null
+    })
+
+    useEffect(() => {
+
+        const getData = async () => {
+            try {
+                setStateFetch((oldValue) => ({ ...oldValue, state: 'loading' }))
+                const res = await fetch(url)
+
+                if (!res.ok) {
+                    setStateFetch({
+                        state: "error",
+                        data: null,
+                        error: Error()
+                    })
+                }
+
+                const data = await res.json()
+
+                setStateFetch({
+                    state: 'success',
+                    data: data,
+                    error: null
+                })
+            } catch (e) {
+                setStateFetch({
+                    state: 'error',
+                    data: null,
+                    error: Error()
+                })
+            }
+
+        }
+        getData()
+
+
+
+    }, [url])
+
+
+    return stateFetch
+}
