d652b9f33461f85fa1e192de2dd657a0def898fa
LopezDev <aragonlopezjuan87@gmail.com>
2023-01-07 20:23:37 +0100

    usar context en la app, iniciar los datod desde server,

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { useContext, useEffect } from 'react'
 import { FormTask } from './components'
 import './App.css'
+import { formValue, task } from './interface'
+import { Context } from './store/store'
+import { useFetch } from './hook'
 
-import { formValue } from './interface'
 
 import { formNewTask } from "./data/formNewTask"
 const onChange: formValue = {
@@ -11,10 +14,38 @@ const onChange: formValue = {
 
 
 function App() {
+  const [state, dispatch] = useContext(Context)
+  console.log("🚀 ~ file: App.tsx:18 ~ App ~ dispatch", dispatch)
+
+  const stateFetch = useFetch('http://localhost:3000/tasks/')
+
+  useEffect(() => {
+    dispatch({ type: 'initialState', payload: stateFetch })
+  }, [stateFetch])
+
+  if (state.state === 'idle' || state.state === 'error') {
+    return <h4>error</h4>
+  }
+  if (state.state === 'loading') {
+    return <h4>...loading</h4>
+  }
+
   return (
-    <div className="App">
-      <FormTask data={formNewTask} handleSubmit={(e) => e.preventDefault()} state='idle' onChange={onChange} />
-    </div>
+    <>
+      <ul>
+        {state.data.map((task: task) => {
+          return (
+            <li key={task.id}>
+              <h3>{task.task}</h3>
+              <h6>{task.folder}</h6>
+            </li>
+          )
+        })}
+      </ul>
+      <div className="App">
+        <FormTask data={formNewTask} handleSubmit={(e) => e.preventDefault()} state='idle' onChange={onChange} />
+      </div>
+    </>
   )
 }
 
