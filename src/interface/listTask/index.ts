4b371baae646a1d45de05a88ac7cc37293bf1e4f
LopezDev <aragonlopezjuan87@gmail.com>
2023-01-07 20:24:38 +0100

    add error a la interface listTasks

--- src/interface/listTask/index.ts
+++ src/interface/listTask/index.ts
@@ -1,6 +1,7 @@
 import { task } from '..'
 
 export interface listTask {
-    tasks: null | task[]
-    state: 'error' | 'loading' | 'idle'
+    tasks: null | task[];
+    state: 'error' | 'loading' | 'idle';
+    error: null | Error
 }
