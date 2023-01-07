a61da2bd3885c0b70334b88c26ea419ed899fea8
LopezDev <aragonlopezjuan87@gmail.com>
2023-01-07 20:25:14 +0100

    store: action initial state

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,21 +1,46 @@
-import { createContext } from 'react'
+import { createContext, useReducer } from 'react'
 import { listTask } from '../interface'
 
-const initialState: listTask = {
+const initialState = {
     state: 'idle',
-    tasks: null
+    tasks: null,
+    error: null
 }
 
-export const Context = createContext(initialState)
+interface action {
+    type: string,
+    payload: any
+
+}
+
+interface contextValue {
+    state: listTask
+    dispatch: action
+}
+
+export const Context = createContext<[state: listTask, dispatch: React.Dispatch<action>]>([initialState, () => { }])
+console.log("🚀 ~ file: store.tsx:22 ~ Context", Context)
 
 
 interface store {
     children: React.ReactNode;
 }
 
+
 export const Store = ({ children }: store) => {
+
+    const [state, dispatch] = useReducer((state: listTask, action): listTask => {
+        switch (action.type) {
+            case 'initialState':
+                return action.payload
+            default:
+                return state
+        }
+    }, initialState)
+
+
     return (
-        <Context.Provider value={initialState}>
+        <Context.Provider value={[state, dispatch]}>
             {children}
         </Context.Provider>
     )
