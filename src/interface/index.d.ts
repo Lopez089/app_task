aa1c1bf2f17788734ad8c9506e4bfecd918550c0
Juan Lopez <aragonlopezjuan87@gmail.com>
2023-01-10 11:01:32 +0100

    export interface taskState, FormValue, taskAction

--- src/interface/index.d.ts
+++ src/interface/index.d.ts
@@ -1,11 +1,14 @@
 import { task } from './tasks/index'
-import { listTask } from './listTask/index'
+import { taskState } from './state/state'
 import { formNewTask, CformNewTask, formValue } from './formNewTask/index'
+import { taskAction } from './state/action'
 
 export type {
     task,
-    listTask,
+    taskState,
     formNewTask,
     CformNewTask,
-    formValue
+    formValue,
+    taskAction,
+
 }
