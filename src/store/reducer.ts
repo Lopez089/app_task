fbd9c945eb2809f0b480c34711d81474c0c4d52f
LopezDev <aragonlopezjuan87@gmail.com>
2023-01-26 09:12:00 +0100

    create reducer addTask

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -11,6 +11,9 @@ export const taskReducer = (state: taskState<task[]>, action: taskAction): taskS
   switch (action.type) {
     case actionType.initialState:
       return action.payload
+    case actionType.addTask:
+      console.log(action)
+      return { ...state, data: [...(state.data as []), action.payload] }
     default:
       return state
   }
