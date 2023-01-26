28ea26d6f3458dfc82b35f85feedfacc829c9bfc
LopezDev <aragonlopezjuan87@gmail.com>
2023-01-26 09:10:51 +0100

    import and export WrapperForm to file index to component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import { ListTasks } from './listTask'
 import { FormTask } from './formTask/formTask'
+import { WrapperForm } from './listTask/wrapperForm/wrapperForm'
 
-export { ListTasks, FormTask }
+export { ListTasks, FormTask, WrapperForm }
