59aceb033e1cfd669901f0a6e26a4776b19f93d4
LopezDev <aragonlopezjuan87@gmail.com>
2023-01-30 08:52:19 +0100

    fix:  cambiar el css de card por cardComplete

--- src/components/atoms/item/item.atoms.tsx
+++ src/components/atoms/item/item.atoms.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import './item.atoms.css'
 import { task } from '../../../models/'
 
-export const Item = (props: task): JSX.Element => {
-  const { task, state } = props
+interface IItem {
+  item: task
+}
 
-  const cardColor = state === 'complete' ? 'card' : ''
+export const Item = ({ item }: IItem): JSX.Element => {
+  const { task, state } = item
+
+  const cardComplete = state === 'complete' ? 'cardComplete' : ''
 
   return (
-    <li className={cardColor}>
+    <li className={`card ${cardComplete}`}>
       <input type="checkbox" checked={state === 'complete'} />
       {task}
     </li>
